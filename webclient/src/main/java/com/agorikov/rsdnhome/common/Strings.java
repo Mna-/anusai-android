6f7e97db73bad912bb49eb1a11cfb1e9f3fad7fe
agorikov <agorikov@gmail.com>
2012-07-11 09:25:51 +0000

    Better name quotes.

--- webclient/src/main/java/com/agorikov/rsdnhome/common/Strings.java
+++ webclient/src/main/java/com/agorikov/rsdnhome/common/Strings.java
@@ -2,13 +2,14 @@ package com.agorikov.rsdnhome.common;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
 
 public class Strings {
-	
-	public static String join(final String delimiter, final String ...items) {
+
+	public static String join(final String delimiter, final String... items) {
 		return join(delimiter, Arrays.asList(items));
 	}
-	
+
 	public static String join(final String delimiter, final Collection<?> items) {
 		final StringBuilder sb = new StringBuilder();
 		for (final Object item : items) {
@@ -19,12 +20,14 @@ public class Strings {
 		}
 		return sb.toString();
 	}
-	
-	public static String joinFormat(final String delimiter, final String format, final String ...items) {
+
+	public static String joinFormat(final String delimiter,
+			final String format, final String... items) {
 		return joinFormat(delimiter, format, Arrays.asList(items));
 	}
-	
-	public static String joinFormat(final String delimiter, final String format, final Collection<String> items) {
+
+	public static String joinFormat(final String delimiter,
+			final String format, final Collection<String> items) {
 		final StringBuilder sb = new StringBuilder();
 		for (final String item : items) {
 			if (sb.length() != 0) {
@@ -35,7 +38,8 @@ public class Strings {
 		return sb.toString();
 	}
 
-	public static String joinPattern(final String delimiter, final String pat, final int number) {
+	public static String joinPattern(final String delimiter, final String pat,
+			final int number) {
 		final StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < number; ++i) {
 			if (sb.length() != 0) {
@@ -46,7 +50,7 @@ public class Strings {
 		return sb.toString();
 	}
 
-	public static String replyInitials(final String s) {
+	public static String replyInitialsOld(final String s) {
 		if (s != null) {
 			final StringBuilder sb = new StringBuilder();
 			final String inpStr = s.replaceAll("\\s", "");
@@ -65,5 +69,62 @@ public class Strings {
 		}
 		return s;
 	}
-		
+
+	private final static HashMap<String, String> nickShortcutCache;
+
+	static {
+		nickShortcutCache = new HashMap<String, String>();
+
+		// Fill in exceptions
+		nickShortcutCache.put("Igor Trofimov", "iT");
+		nickShortcutCache.put("_MarlboroMan_", "_MM_");
+		nickShortcutCache.put("Hacker_Delphi", "H_D");
+	}
+
+	public static String replyInitialsJanus(final String nick) {
+		final String shortcut = nickShortcutCache.get(nick);
+		if (shortcut != null) {
+			return shortcut;
+		}
+
+		// Getting shortcuts for nickname
+		String shortName = "";
+		if (nick.length() <= 3 && !nick.contains(" ")) {
+			// Nick is no longer then 3 symbols
+			shortName = nick.replaceAll("&", "").replaceAll("<", "")
+					.replaceAll(">", "").replaceAll("\"", "")
+					.replaceAll("'", "");
+
+		} else {
+			// Replace weird symbols with spaces.
+			String un = nick.replaceAll("[^A-ZА-Яa-zа-я0-9]+", " ").trim();
+
+			// Remove non-capital characters and digits.
+			if (!un.contains(" ")) {
+				shortName = un.replaceAll("[a-zа-я0-9]+", "");
+				if (shortName.length() > 3)
+					shortName = shortName.substring(0, 3);
+			}
+
+			// if nick is completely non-capital
+			if (shortName.length() == 0) {
+				// Split to words
+				final String[] sa = un.split("\\s+");
+
+				// Use first symbol only
+				for (int i = 0; i < sa.length && i < 3; i++)
+					shortName += sa[i].charAt(0);
+
+				// Make upper case.
+				shortName = shortName.toUpperCase();
+			}
+		}
+
+		if (nickShortcutCache.size() < 200) {
+			// Cache only first 200 nicks
+			nickShortcutCache.put(nick, shortName);
+		}
+		return shortName;
+	}
+
 }
